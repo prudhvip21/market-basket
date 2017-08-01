a761cba9391c9d5a1aba38e4900faee3bf555acc
prudhvi <prudhvi2121991@gmail.com>
2017-08-01 09:27:54 +0000

    pf_list generated

--- periodic_fp_growth.py
+++ periodic_fp_growth.py
@@ -1,5 +1,19 @@
 import itertools
 
+import os
+import time
+os.chdir('/root/mb/market_basket_data')
+from sklearn.metrics import precision_score,accuracy_score,recall_score,f1_score, confusion_matrix
+import numpy as np # linear algebra
+import pandas as pd
+
+order_products_train_df = pd.read_csv("order_products__train.csv")
+order_products_prior_df = pd.read_csv("order_products__prior.csv")
+orders_df = pd.read_csv("orders.csv")
+products_df = pd.read_csv("products.csv")
+aisles_df = pd.read_csv("aisles.csv")
+departments_df = pd.read_csv("departments.csv")
+
 
 class FPNode(object):
     """
@@ -280,21 +294,42 @@ def generate_association_rules(patterns, confidence_threshold):
 
 
 
-def pf_list(orders) :
-    curr = 0
-    for order in orders :
-        curr = curr + 1
+def plist(orders) :
+    pf_list = {}
+    for index,order in enumerate(orders) :
         for product in order :
             if product in pf_list.keys() :
-
-
-
+                pf_list[product]['freq'] += 1
+                new_per = (index+1) - pf_list[product]['ts']
+                if new_per > pf_list[product]['per'] :
+                    pf_list[product]['per'] = new_per
+                pf_list[product]['ts'] = index + 1
 
             else  :
                 d = {}
-                d[freq] = 1
-                d[per] = 1
-                d[ts] = curr
+                d['freq'] = 1
+                d['per'] = index + 1
+                d['ts'] = index + 1
                 pf_list[product] = d
+    for key in pf_list.keys():
+        if len(orders) - pf_list[key]['ts'] > pf_list[key]['per'] :
+            pf_list[key]['per'] = len(orders) - pf_list[key]['ts']
+
+    print pf_list
+    return pf_list
 
 
+prior_with_userids = pd.merge(order_products_prior_df,orders_df,on = 'order_id', how = 'left')
+
+userids_with_orderlist = prior_with_userids.groupby(['user_id','order_id'])['product_id','order_number'].apply(lambda x: x['product_id'].tolist()).reset_index()
+
+single_user_df = userids_with_orderlist[userids_with_orderlist['user_id']==1]
+
+single_user_df = pd.merge(single_user_df,orders_df.iloc[:,[0,3]],on = 'order_id' , how = 'left')
+
+single_user_df = single_user_df.sort_values(by ='order_number')
+
+plist(single_user_df[0])
+
+def prune_plist(pf_list)
+    for key in pf_list
