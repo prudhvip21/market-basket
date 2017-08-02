d5e423caacff9cc11ddb8f5b533f2003f5c1d727
prudhvi <prudhvi2121991@gmail.com>
2017-08-02 15:09:13 +0530

    testing fp tree

--- periodic_fp_growth.py
+++ periodic_fp_growth.py
@@ -2,7 +2,8 @@ import itertools
 
 import os
 import time
-os.chdir('/root/mb/market_basket_data')
+# os.chdir('/root/mb/market_basket_data')
+os.chdir('/home/prudhvi/Documents/market_basket_data')
 from sklearn.metrics import precision_score,accuracy_score,recall_score,f1_score, confusion_matrix
 import numpy as np # linear algebra
 import pandas as pd
@@ -58,6 +59,90 @@ class FPNode(object):
 
 
 
+# frequent items with key value pairs required
+
+
+class FPTree(object):
+    """
+    A frequent pattern tree.
+    """
+
+    def __init__(self, transactions,root_value, root_count):
+        """
+        Initialize the tree.
+        """
+        #self.frequent = self.find_frequent_items(transactions, threshold)
+        self.headers = self.build_header_table(self.frequent)
+        self.root = self.build_fptree(transactions, root_value,
+            root_count, self.frequent, self.headers)
+
+    def build_header_table(frequent):
+        """
+        Build the header table.
+        """
+        headers = {}
+        for key in frequent.keys():
+            headers[key] = None
+
+        return headers
+
+    def build_fptree(self, transactions, root_value, root_count, frequent, headers):
+        """
+        Build the FP tree and return the root node.
+        """
+        root = FPNode(root_value, root_count, None)
+
+        for transaction in transactions:
+            sorted_items = [x for x in transaction if x in frequent]
+            sorted_items.sort(key=lambda x: frequent[x], reverse=True)
+            if len(sorted_items) > 0:
+                self.insert_tree(sorted_items, root, headers)
+
+        return root
+
+    def insert_tree(self, items, node, headers):
+        """
+        Recursively grow FP tree.
+        """
+        first = items[0]
+        child = node.get_child(first)
+        if child is not None:
+            child.count += 1
+        else:
+            # Add new child.
+            child = node.add_child(first)
+
+            # Link it to header structure.
+            if headers[first] is None:
+                headers[first] = child
+            else:
+                current = headers[first]
+                while current.link is not None:
+                    current = current.link
+                current.link = child
+
+        # Call function recursively.
+        remaining_items = items[1:]
+        if len(remaining_items) > 0:
+            self.insert_tree(remaining_items, child, headers)
+
+
+    def tree_has_single_path(self, node):
+        """
+        If there is a single path in the tree,
+        return True, else return False.
+        """
+        num_children = len(node.children)
+        if num_children > 1:
+            return False
+        elif num_children == 0:
+            return True
+        else:
+            return True and self.tree_has_single_path(node.children[0])
+
+
+
+
 
 def plist(orders) :
     pf_list = {}
@@ -86,9 +171,11 @@ def plist(orders) :
 
 prior_with_userids = pd.merge(order_products_prior_df,orders_df,on = 'order_id', how = 'left')
 
-userids_with_orderlist = prior_with_userids.groupby(['user_id','order_id'])['product_id','order_number'].apply(lambda x: x['product_id'].tolist()).reset_index()
+single_user_df = prior_with_userids[prior_with_userids['user_id']==1]
 
-single_user_df = userids_with_orderlist[userids_with_orderlist['user_id']==1]
+userids_with_orderlist = single_user_df.groupby(['user_id','order_id'])['product_id','order_number'].apply(lambda x: x['product_id'].tolist()).reset_index()
+qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
+qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
 
 single_user_df = pd.merge(single_user_df,orders_df.iloc[:,[0,3]],on = 'order_id' , how = 'left')
 
